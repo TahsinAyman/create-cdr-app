2107288a090e7faf271d513dd1f5fbfbeb9a9da5
fullstackltd <fullstackltd@gmail.com>
2023-11-23 09:03:57 +0600

    inital commit

---
+++ README.md
@@ -0,0 +1,37 @@
+<img src="https://fullstackbd.com/assets/images/logo-monogram.png" height="80" />
+
+<br />
+
+# Component Driven Redux
+
+```bash
+Usage: cdr [options] [command]
+
+Component Driven Redux Framework
+
+Options:
+  -V, --version                 output the version number
+  -h, --help                    display help for command
+
+Commands:
+  new [options] <project-name>  Create a Project
+  generate|g <project-name>     Create a Module For The Project
+  help [command]                display help for command
+```
+
+Component Driven Redux is a Framework which combines the power of multiple powerful tools. they are `TypeScript`, `Vite`, `React`, `Redux`.
+Our Framework focuses on making a React App fast but keeping sure that it is managable by implementing Redux Framework and TypeScript for better completeion and type safety and it uses Vite to bundle JavaScript Faster.
+
+The Main Packages those were used is:
+
+- `react`
+- `redux`
+- `@reduxjs/toolkit`
+- `react-hook-form`
+- `yup`
+- `@hookform/resolvers`
+- `react-router-dom`
+- `vite`
+- `typescript`
+
+This is a prereleased version the main Version is in devlopment. currently there are lots of lacking like _`Testing`_, _`Automation`_ and etc. but the core components are complete
---
+++ src/index.js
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+import { program } from "commander";
+import ora from "ora";
+import simpleGit from "simple-git";
+import path from "path";
+import fs from "fs-extra";
+
+program.version("1.0.0").description("Component Driven Redux Framework");
+
+program
+  .command("new <project-name>")
+  .option(
+    "-t --template <name>",
+    "Specify Template Name like tailwindcss, bootstrap"
+  )
+  .description("Create a Project")
+  .action(async (name, { template }) => {
+    var repo = "";
+    if (template) {
+      repo = "https://github.com/TahsinAyman/cdr-template-" + template;
+    } else {
+      repo = "https://github.com/TahsinAyman/cdr-template";
+    }
+    const spinner = ora("Creating The Project").start();
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const git = simpleGit();
+    git.clone(
+      "https://github.com/TahsinAyman/cdr-template",
+      name,
+      async (err) => {
+        if (err) {
+          spinner.fail(`Error Creating Project: ${err}`);
+        } else {
+          fs.remove(path.join(name, ".git"), (err) => {
+            if (err) {
+              spinner.fail(`Error Creating Project: ${err}`);
+            } else {
+              spinner.succeed(`Successfully Created Project`);
+            }
+          });
+        }
+      }
+    );
+  });
+
+program
+  .command("generate <project-name>")
+  .alias("g")
+  .description("Create a Module For The Project")
+  .action(async (name) => {
+    const spinner = ora("Creating Module").start();
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const git = simpleGit();
+    const directory = path.join("src/app", name);
+    git.clone(
+      "https://github.com/TahsinAyman/cdr-module-template",
+      directory,
+      async (err) => {
+        if (err) {
+          spinner.fail(`Error Creating Module: ${err}`);
+        } else {
+          fs.remove(path.join(name, ".git"), (err) => {
+            if (err) {
+              spinner.fail(`Error Creating Module: ${err}`);
+            } else {
+              spinner.succeed(`Successfully Created Module`);
+            }
+          });
+        }
+      }
+    );
+  });
+
+program.parse(process.argv);
